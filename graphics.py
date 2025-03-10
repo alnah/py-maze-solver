6dea58bc8f940c5eef96c18324d26ff6c3699aa8
alnah <alexis.nahan@gmail.com>
2025-03-10 20:29:42 -0300

    fix: convert int to float types

--- graphics.py
+++ graphics.py
@@ -4,12 +4,13 @@ from tkinter import Tk, BOTH, Canvas
 
 class FillColor(Enum):
     BLACK = "black"
+    WHITE = "white"
     RED = "red"
     GREY = "grey"
 
 
 class Point:
-    def __init__(self, x: int, y: int) -> None:
+    def __init__(self, x: float, y: float) -> None:
         self.x = x
         self.y = y
 
@@ -31,13 +32,18 @@ class Line:
 
 
 class Window:
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: float, height: float):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(
+            self.__root,
+            bg=FillColor.WHITE.value,
+            height=height,
+            width=width,
+        )
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self) -> None:
         self.__root.update_idletasks()
@@ -49,8 +55,8 @@ class Window:
             self.redraw()
         print("window closed...")
 
-    def close(self) -> None:
-        self.__running = False
-
     def draw_line(self, line: Line, fill_color: FillColor = FillColor.BLACK) -> None:
         line.draw(self.__canvas, fill_color)
+
+    def close(self) -> None:
+        self.__running = False
