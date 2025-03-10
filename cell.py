91a7ae2979f7ffb49e47383e303899f041f52691
alnah <alexis.nahan@gmail.com>
2025-03-10 14:03:00 -0300

    feat: draw moves between cells

--- cell.py
+++ cell.py
@@ -1,4 +1,4 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, FillColor
 
 
 class Cell:
@@ -34,3 +34,25 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell: "Cell", undo: bool = False) -> None:
+        if not self._x1 or not self._x2 or not self._y1:
+            return
+
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
+
+        if not to_cell._x1 or not to_cell._x2 or not to_cell._y1:
+            return
+
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
+
+        fill_color = FillColor.RED
+        if undo:
+            fill_color = FillColor.GREY
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color)
