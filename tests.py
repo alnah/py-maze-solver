ff59b5f68bc1afdf0ea3323505ccfa0fddb8597d
alnah <alexis.nahan@gmail.com>
2025-03-10 22:00:18 -0300

    test: define specs for entrance and exit

--- tests.py
+++ tests.py
@@ -3,6 +3,11 @@ from maze import Maze
 
 
 class Tests(unittest.TestCase):
+    def test_empty_maze(self):
+        num_cols, num_rows = 0, 0
+        maze = Maze(0, 0, num_cols, num_rows, 10, 10)
+        self.assertEqual(len(maze._cells), num_cols)
+
     def test_maze_create_cells(self):
         testCases = [
             {
@@ -27,9 +32,12 @@ class Tests(unittest.TestCase):
             self.assertEqual(len(maze._cells), tc["num_cols"])
             self.assertEqual(len(maze._cells[0]), tc["num_rows"])
 
-    def test_empty_maze(self):
-        maze = Maze(0, 0, 0, 0, 10, 10)
-        self.assertEqual(len(maze._cells), 0)
+    def test_broken_entrance_and_exit(self):
+        num_cols, num_rows = 10, 10
+        maze = Maze(0, 0, num_rows, num_cols, 10, 10)
+        entrance, exit = maze._cells[0][0], maze._cells[-1][-1]
+        self.assertFalse(entrance.has_top_wall)
+        self.assertFalse(exit.has_bottom_wall)
 
 
 if __name__ == "__main__":
